9906a8b900ee129f7a5abd875b51beb91ecd5e56
Chien Pham <Chien.VuPham@coxautoinc.com>
2021-02-16 21:52:54 +0700

    Add linkerd

--- kind/run-cluster.sh
+++ kind/run-cluster.sh
@@ -2,14 +2,22 @@
 
 set -xe
 
+# kind v0.9.0
 kind delete cluster || true
 kind create cluster --config ./kind-config.yaml
 
+# linkerd version edge-21.2.2
+linkerd check --pre
+linkerd install | kubectl apply -f -
+linkerd check
+linkerd viz install | kubectl apply -f -
+
+#kubectl v1.19.4
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
+  --timeout=180s
 
 kubectl apply -f ../k8s
